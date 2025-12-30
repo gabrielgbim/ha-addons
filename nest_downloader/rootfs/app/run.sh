c4518b2a4179861aea414213a9116a15ba50e5c0
gabrielgbim <gabrielgbim@gmail.com>
2025-12-30 00:49:28 -0500

    Trying to fix python start application

--- nest_downloader/rootfs/app/run.sh
+++ nest_downloader/rootfs/app/run.sh
@@ -26,4 +26,5 @@ bashio::log.info "Refresh interval: ${REFRESH_INTERVAL} minutes"
 
 # Start Python application
 bashio::log.info "Starting Python application..."
-python main.py
+cd /app || bashio::exit.nok "Failed to change to /app directory"
+python3 -u main.py || bashio::exit.nok "Python application failed"
